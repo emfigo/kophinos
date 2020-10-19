0844c8c69bd252973f14e2779e3543273f051ab4
emfigo <emfigo@gmail.com>
2020-10-19 11:22:06 +0200

    👾 Add project structure with DB

---
+++ app.py
@@ -0,0 +1,14 @@
+import sys
+from kophinos import app
+
+def main(argv):
+    if argv[1] == 'api':
+        app.run()
+    else:
+        print('Sorry no valid option given')
+        exit(-1)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
+
