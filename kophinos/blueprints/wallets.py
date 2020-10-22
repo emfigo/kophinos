f3497d639fd3bc7a032e2f8a65e967f446fdcd7a
emfigo <emfigo@gmail.com>
2020-10-22 11:44:18 +0200

    💵 Add transfer endpoint

--- kophinos/blueprints/wallets.py
+++ kophinos/blueprints/wallets.py
@@ -3,10 +3,11 @@ import flask_login
 from http import HTTPStatus
 
 from kophinos.blueprints import helpers
-from kophinos.exceptions import InvalidTransaction
+from kophinos.exceptions import InvalidCurrency, InvalidTransfer, InvalidTransaction, InvalidUser, InvalidWallet
 from kophinos.models.user import User
 from kophinos.models.transaction import Transaction
 from kophinos.services.transaction_create import TransactionCreate
+from kophinos.services.transfer_create import TransferCreate
 from kophinos.services.wallet_create import WalletCreate
 
 wallets = Blueprint('wallets', __name__)
@@ -73,6 +74,35 @@ def create_wallet_transactions(currency):
             response = TransactionCreate.call(wallet, request.json).as_dict()
         except InvalidTransaction:
             response = 'Invalid transaction. Please change transaction details.'
+            response_code = HTTPStatus.BAD_REQUEST
+
+    return jsonify(response), response_code
+
+@wallets.route('/wallets/<currency>/transfers', methods=['POST'])
+@flask_login.login_required
+def create_wallet_transfer(currency):
+    user_authentication_details = flask_login.current_user
+    user = User.find_by_id(user_authentication_details.user_id)
+    wallet = helpers.get_wallet(user, currency)
+    response_code = HTTPStatus.CREATED
+
+    if type(wallet) is str:
+        response = wallet
+        response_code = HTTPStatus.BAD_REQUEST
+    else:
+        try:
+            response = TransferCreate.call(user, wallet, request.json).as_dict()
+        except InvalidTransaction:
+            response = 'Invalid transaction. Please change transaction details.'
+            response_code = HTTPStatus.BAD_REQUEST
+        except InvalidTransfer:
+            response = 'Invalid transfer. Please change transfer details.'
+            response_code = HTTPStatus.BAD_REQUEST
+        except InvalidUser:
+            response = 'Invalid receiver. Please specify a valid receiver email.'
+            response_code = HTTPStatus.BAD_REQUEST
+        except (InvalidWallet, InvalidCurrency):
+            response = "Invalid receiver wallet. Receiver can't accept transfer with specified currency."
             response_code = HTTPStatus.BAD_REQUEST
 
     return jsonify(response), response_code
--- tests/integration/blueprints/test_wallets.py
+++ tests/integration/blueprints/test_wallets.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 import pytest
 
 from kophinos.models.currency import Currency
+from kophinos.models.transfer import Transfer
 from kophinos.models.wallet import Wallet
 from kophinos.models.transaction import Transaction
 from kophinos.services.user_create import UserCreate
@@ -15,6 +16,13 @@ class TestBlueprintWallets:
         'password': 'Som3r3allyD1ff1cultPa$$word!'
     }
 
+    receiver_details = {
+        'first_name': 'test2',
+        'last_name': 'Test',
+        'email': 'test2.test@reallycool.test',
+        'password': 'Som3r3allyD1ff1cultPa$$word!'
+    }
+
     wallet_details = {
         'currency': Currency.SGD.name
     }
@@ -22,6 +30,12 @@ class TestBlueprintWallets:
     transaction_details = {
         'amount_cents': 100000,
         'type': 'CREDIT'
+    }
+
+    transfer_details = {
+        "receiver_email": "test2.test@reallycool.test",
+        "amount_cents": 100000,
+        "currency": Currency.SGD.name
     }
 
     def test_creates_wallet_when_all_details_are_valid(self, testapp, database):
@@ -257,6 +271,174 @@ class TestBlueprintWallets:
         client = testapp.test_client()
 
         response = client.get(f"/wallets/{self.wallet_details['currency']}/transactions")
+
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json == 'unauthorized'
+
+    def test_creates_expected_wallet_transfer_when_authenticated_and_valid_details(self, testapp, database):
+        client = testapp.test_client()
+
+        client.post('/users', json=self.receiver_details)
+        response = client.post('/login', json=self.receiver_details)
+
+        headers = {
+            'Authorization': f"Basic {response.json}"
+        }
+
+        client.post('/wallets', headers=headers, json=self.wallet_details)
+
+        client.post('/users', json=self.user_details)
+        response = client.post('/login', json=self.user_details)
+
+        headers = {
+            'Authorization': f"Basic {response.json}"
+        }
+
+        response = client.post('/wallets', headers=headers, json=self.wallet_details)
+
+        wallet = Wallet.query.filter_by(id = response.json['id']).first()
+
+        response = client.post(f"/wallets/{self.wallet_details['currency']}/transfers", headers=headers, json=self.transfer_details)
+
+        assert Transfer.query.count() == 1
+
+        wallet = Wallet.query.filter_by(id = wallet.id).first()
+        wallet_dict = wallet.as_dict()
+        wallet_dict['id'] = str(wallet_dict['id'])
+
+        assert response.status_code == HTTPStatus.CREATED
+        assert response.json == wallet_dict
+
+    def test_returns_error_message_when_authenticated_and_non_existing_wallet_transfer(self, testapp, database):
+        client = testapp.test_client()
+
+        client.post('/users', json=self.user_details)
+        response = client.post('/login', json=self.user_details)
+
+        headers = {
+            'Authorization': f"Basic {response.json}"
+        }
+
+        response = client.post(f"/wallets/{self.wallet_details['currency']}/transfers", headers=headers, json=self.transfer_details)
+
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+        assert response.json == 'Invalid account. Account does not exist for user.'
+
+    @pytest.mark.parametrize('invalid_detail', [
+        {'amount_cents': '1000000' },
+        {'amount_cents': -1000000 },
+        {'amount_cents': 'hello' }
+    ])
+    def test_returns_error_message_when_authenticated_and_invalid_transfer_details(self, testapp, database, invalid_detail):
+        client = testapp.test_client()
+
+        client.post('/users', json=self.receiver_details)
+        response = client.post('/login', json=self.receiver_details)
+
+        headers = {
+            'Authorization': f"Basic {response.json}"
+        }
+
+        client.post('/wallets', headers=headers, json=self.wallet_details)
+
+        client.post('/users', json=self.user_details)
+        response = client.post('/login', json=self.user_details)
+
+        headers = {
+            'Authorization': f"Basic {response.json}"
+        }
+
+        client.post('/wallets', headers=headers, json=self.wallet_details)
+
+        response = client.post(f"/wallets/{self.wallet_details['currency']}/transfers", headers=headers, json={ **self.transfer_details, **invalid_detail} )
+
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+        assert response.json == 'Invalid transaction. Please change transaction details.'
+
+    @pytest.mark.parametrize('invalid_detail', [
+        {'amount_cents': None },
+        {'receiver_email': None },
+        {'currency': None }
+    ])
+    def test_returns_error_message_when_authenticated_and_missing_mandatory_details(self, testapp, database, invalid_detail):
+        client = testapp.test_client()
+
+        client.post('/users', json=self.receiver_details)
+        response = client.post('/login', json=self.receiver_details)
+
+        headers = {
+            'Authorization': f"Basic {response.json}"
+        }
+
+        client.post('/wallets', headers=headers, json=self.wallet_details)
+
+        client.post('/users', json=self.user_details)
+        response = client.post('/login', json=self.user_details)
+
+        headers = {
+            'Authorization': f"Basic {response.json}"
+        }
+
+        client.post('/wallets', headers=headers, json=self.wallet_details)
+
+        response = client.post(f"/wallets/{self.wallet_details['currency']}/transfers", headers=headers, json={ **self.transfer_details, **invalid_detail} )
+
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+        assert response.json == 'Invalid transfer. Please change transfer details.'
+
+    @pytest.mark.parametrize('invalid_detail', [
+        {'receiver_email': 'nonexisting_email@email.test' }
+    ])
+    def test_returns_error_message_when_authenticated_and_invalid_receiver(self, testapp, database, invalid_detail):
+        client = testapp.test_client()
+
+        client.post('/users', json=self.receiver_details)
+        response = client.post('/login', json=self.receiver_details)
+
+        headers = {
+            'Authorization': f"Basic {response.json}"
+        }
+
+        client.post('/wallets', headers=headers, json=self.wallet_details)
+
+        client.post('/users', json=self.user_details)
+        response = client.post('/login', json=self.user_details)
+
+        headers = {
+            'Authorization': f"Basic {response.json}"
+        }
+
+        client.post('/wallets', headers=headers, json=self.wallet_details)
+
+        response = client.post(f"/wallets/{self.wallet_details['currency']}/transfers", headers=headers, json={ **self.transfer_details, **invalid_detail} )
+
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+        assert response.json == 'Invalid receiver. Please specify a valid receiver email.'
+
+    def test_returns_error_message_when_authenticated_and_invalid_receiver_wallet(self, testapp, database):
+        client = testapp.test_client()
+
+        client.post('/users', json=self.receiver_details)
+        response = client.post('/login', json=self.receiver_details)
+
+        client.post('/users', json=self.user_details)
+        response = client.post('/login', json=self.user_details)
+
+        headers = {
+            'Authorization': f"Basic {response.json}"
+        }
+
+        client.post('/wallets', headers=headers, json=self.wallet_details)
+
+        response = client.post(f"/wallets/{self.wallet_details['currency']}/transfers", headers=headers, json=self.transfer_details )
+
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+        assert response.json == "Invalid receiver wallet. Receiver can't accept transfer with specified currency."
+
+    def test_returns_error_message_transfer_when_not_authenticated(self, testapp, database):
+        client = testapp.test_client()
+
+        response = client.post(f"/wallets/{self.wallet_details['currency']}/transfers", json=self.transfer_details)
 
         assert response.status_code == HTTPStatus.UNAUTHORIZED
         assert response.json == 'unauthorized'
